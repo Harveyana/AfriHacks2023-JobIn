4537e55e0ed94170ce91934cc78afcb41cfe9d7b
Harvey Ana <harveyana3@gmail.com>
2023-12-10 09:47:35 +0100

    Layout Completed

---
+++ components/baseTab.vue
@@ -0,0 +1,11 @@
+<template>
+    <NuxtLink :href="props.url" data-aos="fade-left" data-aos-once="true" class="w-full text-xs text-[#71787b] group bg-white flex flex-row items-center justify-start rounded-xl py-2 px-2 dark:bg-transparent dark:text-[#71787b] hover:bg-[#f3f3f3] dark:hover:bg-[#1e2328] dark:hover:text-white hover:text-black">
+      <slot></slot>
+    </NuxtLink>
+  </template>
+
+  <script setup lang="ts">
+  const props = defineProps<{
+      url: string
+  }>()
+  </script>
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="h-screen dark:bg-[#0b1015] max-h-[50rem] border-2 border-black max-w-screen-xl flex flex-wrap flex-row mx-auto overflow-hidden">
+      <SideBar />
+      <section class="h-full w-full overflow-y-scroll scroll-smooth w-full sm:w-[82%] grid grid-cols-12 pt-4 gap-x-2">
+        <div class="col-span-12 w-full overflow-y-scroll scroll-smooth">
+          <slot/>
+        </div>
+      </section>
+    </div>
+  </template>
