6fa9ed02ef2dfad0c37e392028c18a6cd68a27fa
Harvey Ana <harveyana3@gmail.com>
2024-02-21 17:39:34 +0100

    popup fix

--- components/toolbar.vue
+++ components/toolbar.vue
@@ -11,11 +11,12 @@
               <svg class="w-6 md:w-7 dark:stroke-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M10.146 3.248a2 2 0 0 1 3.708 0A7.003 7.003 0 0 1 19 10v4.697l1.832 2.748A1 1 0 0 1 20 19h-4.535a3.501 3.501 0 0 1-6.93 0H4a1 1 0 0 1-.832-1.555L5 14.697V10c0-3.224 2.18-5.94 5.146-6.752zM10.586 19a1.5 1.5 0 0 0 2.829 0h-2.83zM12 5a5 5 0 0 0-5 5v5a1 1 0 0 1-.168.555L5.869 17H18.13l-.963-1.445A1 1 0 0 1 17 15v-5a5 5 0 0 0-5-5z"/></svg>
             </span>
             <img v-if="user" @click="toggle" :src="user.photoUrl" class="w-10 cursor-pointer p-1 rounded-[50px] bg-green-300 mx-3 md:my-1 md:w-8" />
-            <span v-if="user" @click="toggle" class="cabinet cursor-pointer truncate text-xs text-black dark:text-white">
+            <!-- <span v-if="user" @click="toggle" class="cabinet cursor-pointer truncate text-xs text-black dark:text-white">
               {{user.displayName}}
             </span>
-            <svg class=' cursor-pointer w-6 md:w-7 dark:fill-white dark:stroke-white' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6l1.41-1.42Z"/></svg>
-            <Menu ref="menu" class="border-none" :model="items" :popup="true" :unstyled="true" :pt="{
+            <svg class=' cursor-pointer w-6 md:w-7 dark:fill-white dark:stroke-white' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6l1.41-1.42Z"/></svg> -->
+
+            <!-- <Menu ref="menu" class="border-none" :model="items" :popup="true" :unstyled="true" :pt="{
                   root: {
                       style: 'background:white;width:7rem;'
                   },
@@ -25,7 +26,64 @@
                   menuitem:{
                     class:'cabinet hover:bg-gray-200 px-2 py-1 cursor-pointer'
                   }
-              }" />
+              }" /> -->
+
+              <DropdownMenuRoot v-model:open="toggleState">
+                  <DropdownMenuTrigger
+                    class="rounded-full w-fit h-fit inline-flex items-center justify-center text-grass11 bg-white outline-none hover:bg-green3"
+                    aria-label="dropdown menu options"
+                  >
+                    <span v-if="user" @click="toggle" class="cabinet cursor-pointer truncate text-xs text-black dark:text-white">
+                      {{user.displayName}}
+                    </span>
+                    <svg class=' cursor-pointer w-6 md:w-7 dark:fill-white dark:stroke-white' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6l1.41-1.42Z"/></svg>
+                  </DropdownMenuTrigger>
+
+                  <DropdownMenuPortal>
+                    <DropdownMenuContent
+                      class="min-w-[150px] z-20 mx-4 outline-none bg-white rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
+                      :side-offset="10"
+                    >
+                      <DropdownMenuItem
+                        value="Settings"
+                        class="hidden sm:flex group text-sm cursor-pointer leading-none text-black rounded-[3px] items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-gray-200 data-[highlighted]:text-black"
+                        @click="$router.push('/account')"
+                      >
+                        Settings
+                        
+                      </DropdownMenuItem>
+
+                      <DropdownMenuItem
+                        value="Settings"
+                        class=" sm:hidden group text-sm cursor-pointer leading-none text-black rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-gray-200 data-[highlighted]:text-black"
+                        @click="$router.push('/more')"
+                      >
+                        Settings
+                        
+                      </DropdownMenuItem>
+
+                      <DropdownMenuItem
+                        value="Log Out"
+                        class="group text-sm cursor-pointer leading-none text-black rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-gray-200 data-[highlighted]:text-black"
+                        @click="logout"
+                      >
+                        Logout
+                        <!-- <div
+                          class="ml-auto pl-[20px] text-mauve11 group-data-[highlighted]:text-white group-data-[disabled]:text-mauve8"
+                        >
+                          ⌘+T
+                        </div> -->
+                      </DropdownMenuItem>
+                      
+                      
+                      <DropdownMenuSeparator class="h-[1px] bg-green6 m-[5px]" />
+                      
+                      
+                      <DropdownMenuArrow class="fill-white" />
+
+                    </DropdownMenuContent>
+                  </DropdownMenuPortal>
+                </DropdownMenuRoot>
           </div>
   </template>
 
@@ -35,6 +93,7 @@
 
 
   const state = useGlobalState()
+  const toggleState = ref(false)
 
   const menu = ref();
   const router = useRouter()
@@ -43,7 +102,14 @@
   //   user:user
   // }>()
   const user = state.user
-
+  const logout = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      router.push('/auth')
+      }).catch((error) => {
+        console.log(error)
+      });
+  }
   const toggle = (event: any) => {
       menu.value.toggle(event);
   };
